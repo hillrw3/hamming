fec8f8cf86551a3f584a7174993dce5f02aa7e60
Rob Hill <robert.hill0122@gmail.com>
2014-06-18 15:17:13 -0600

    Hamming it up

--- hamming.rb
+++ hamming.rb
@@ -1,8 +1,19 @@
 require_relative "testing_library"
 
 def hamming(strand_1, strand_2)
-
+  split1 = strand_1.split(//)
+  split2 = strand_2.split(//)
+  if split1.length > split2.length
+    split1.pop
+  elsif split2.length > split1.length
+    split2.pop
+  else
+  end
+  differences = split1 - split2
+  differences.length
 end
+
+
 
 check("Hamming distance between identical strands",
       hamming("A", "A") == 0)
@@ -24,3 +35,9 @@ check("Ignoring extra length in the first strand when it's longer",
 
 check("Ignoring extra length in the second strand when it's longer",
       hamming("AAA", "AAAG") == 0)
+
+#break strands apart
+#compare corresponding string value
+#calculate number of differences
+
+
